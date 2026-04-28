16dbc97a436467f0c7d4c5c9abb806ed2e7142fd
OC-46 <owen.collins@unh.edu>
2026-04-28 22:03:36 +0000

    added a priority queue with a heap

--- src/flashcard/main.py
+++ src/flashcard/main.py
@@ -1,20 +1,39 @@
+import heapq
 import random
 from .deck import load_deck, save_deck
 
 
-def run_session(card_dict, deck):
+def update_priority(card_dict, deck, card_id, correct):
+    card = card_dict[card_id]
+    if correct:
+        card["priority"] = max(0, card["priority"] - 1)
+    else:
+        card["priority"] += 2
+
+    heapq.heappush(deck, (card["priority"], card_id))
+    
+
+
+def run_session(card_dict, heap, filepath):
     print("Welcome to my flashcard app. Type 'quit' to exit.\n")
 
-    while deck:
-        card_id = deck.pop(0)
+    while heap:
+        priority, card_id = heapq.heappop(heap)
         card = card_dict[card_id]
+
+        if card["priority"] > 0:
+            heapq.heappush(heap, (card["priority"], card_id))
+            continue
 
         print(f"Question: {card['question']}")
         user_input = input("\nPress Enter to see the answer (or type 'quit' to exit): ").strip().lower()
 
         if user_input == "quit":
-            print("Ending session")
+            heapq.heappush(heap, (card["priority"], card_id))
+            print("saving and exiting...")
+            save_deck(filepath, card_dict)
             break
+
 
         print(f"\nAnswer: {card['answer']}")
 
@@ -22,25 +41,26 @@ def run_session(card_dict, deck):
         while result not in ("y", "n"):
             result = input("please enter y or n: ").strip().lower()
 
-        if result == "y":
-            print("good job!\n")
+
+        if got_right := (result == "y"):
+            update_priority(card_dict, heap, card_id, True)
+
+        if got_right:
+             print("good job!\n")
         else:
             print("you'll get it next time!\n")
 
         print("-"*40 + "\n")
 
-    if not deck:
-        print("you have completed the whole deck!")
+    save_deck(filepath, card_dict)
+    print("No more cards to review. Good job! Exiting...")
 
 
 
 def main():
     filepath = "../flashcards.JSON"
-    card_dict, deck = load_deck(filepath)
-
-    deck_list = [card_id for _, card_id in deck]
-    
-    run_session(card_dict, deck_list)
+    card_dict, heap = load_deck(filepath)
+    run_session(card_dict, heap, filepath)
 
 
 if __name__ == "__main__":
